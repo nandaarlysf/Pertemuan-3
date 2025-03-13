02fd36d1ea02132810808bad0b95cdfae4f2d70d
nandaarlysf <nandaarlys36@gmail.com>
2025-03-13 12:05:35 +0700

    variable global

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,3 +1,7 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+int arr[20];          //Deklarasi variable global array a dengan ukuran 20
+int n ;               //Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
+
