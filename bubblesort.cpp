60bba0d7c98497152ec70a6a7d56bd9489416dc7
nandaarlysf <nandaarlys36@gmail.com>
2025-03-13 12:21:47 +0700

    prosedur display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int arr[20];          //Deklarasi variable global array a dengan ukuran 20
 int n ;               //Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
 
-void input(){        //procedur untuk  input
+void input()
+{
+    //procedur untuk  input
     while ((true)){
         cout<<"Masukkan banyaknya elemen pada array:"<<endl;
         cin>> n;
@@ -15,7 +17,7 @@ void input(){        //procedur untuk  input
                 cout<<"\nArray dapat mempunyai maksimal 20 elemen\n"<< endl;
               }   
     }
-    cout<<endl
+    cout<<endl;
     cout<<"==================="<<endl;
     cout<<"Masukkan elemen array"<<endl;
     cout<<"====================="<<endl;
@@ -24,23 +26,47 @@ void input(){        //procedur untuk  input
         cout << "Data ke-"<< (i+1) <<";";
         cin >> arr[i];
     }
+}       
 
+void bubbleSortArray(){
+    int pass = 1; //step 1
 
-    void bubbleSortArray(){
-        int pass=1;  //step1
-
-        do
-    }
-    for(int j=0; j<=n -1-pass;j++)
-    { //step 2
-
-        if ( arr[j]>arr[j+1])//Step 3
-        {
+    do
+    {//step 2
+        for (int j = 0; j <= n -1-pass;j++) //step 3
+    {
+        if (arr[j] > arr[j+1]){
             int temp;
-            temp=arr[j]
-            arr [j+1]=temp;
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
         }
-    }
 
-pass=pass+1; //step 4
-}while (pass<=n-1 );// step 5
+        pass = pass + 1; //step 4
+    }
+        
+
+    }while (pass <= n - 1); //step 5
+}
+
+void display ()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        cout << " --> ";
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
+
